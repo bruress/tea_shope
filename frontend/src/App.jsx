91aee4ae5dad916b4c58a77c5c5a9c04924b0fba
bruress <hualian.my.hualian@gmail.com>
2026-01-07 00:14:46 +0300

    add login and registr

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,19 +9,21 @@ import {
 import React from 'react'
 import { useState, useEffect } from 'react'
 import axios from 'axios';
-
+import Login from './pages/Login';
+import Register from './pages/Register';
 import Home from './pages/Home'
 
 function App() {
-
+  const [user, setUser] = useState(null);
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home/>}
-        />
+        <Route path="/login" element={<Login setUser={setUser}/>}/>
+        <Route path="/register" element={<Register setUser={setUser}/>}/>
+        <Route path="/" element={<Home user={user}/>}/>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,124 @@
+import React, { useState } from "react";
+import { Logo } from "../assets/Images";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+const API_URL = import.meta.env.VITE_API_URL;
+
+const Register = ({setUser}) => {
+    const [form, setForm] = useState({
+        user_name:"",
+        email:"",
+        telephone_number:"",
+        password:"",
+    });
+    const [error, setError] = useState("");
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const res = await axios.post(`${API_URL}/api/auth/register`, form);
+            setUser(res.data.user);
+            navigate("/");
+        }
+        catch (err) {
+            setError("Registration failed");
+        }
+    };
+    
+    return (
+        <div className="absolute bg-cover bg-[url(src/assets/img/back-3.png)] w-full min-h-screen">
+            <form onSubmit={handleSubmit}>
+                <div className="flex justify-center items-center mt-[30px]">
+                    <Logo/>
+                    <label className="text-[48px] font-cormorant text-[#0D3B24] ml-[8px]">TEA SHOP</label>
+                </div>
+                <div className="mt-[190px] flex flex-col justify-center items-center">
+                    <label className="text-[128px] font-cormorant text-[#0D3B24] font-semiboldbold">Sign up</label>
+                    <input
+                        className="
+                        bg-[#FFFCF7]
+                        border-[#0D3B24]
+                        border-[1px]
+                        rounded-[15px]
+                        w-[550px]
+                        h-[50px]
+                        text-center
+                        font-lora
+                        text-[24px]
+                        text-[#0D3B24]
+                        outline-none
+                        mt-[15px]"
+                        placeholder="name"
+                        type="text"
+                        value={form.user_name}
+                        onChange={(e) => setForm({...form, user_name: e.target.value})}
+                    />
+                    <input
+                        className="
+                        bg-[#FFFCF7]
+                        border-[#0D3B24]
+                        border-[1px]
+                        rounded-[15px]
+                        w-[550px]
+                        h-[50px]
+                        text-center
+                        font-lora
+                        text-[24px]
+                        text-[#0D3B24]
+                        outline-none
+                        mt-[15px]"
+                        placeholder="telephone number"
+                        type="telephone"
+                        value={form.telephone_number}
+                        onChange={(e) => setForm({...form, telephone_number: e.target.value})}
+                    />
+                    <input className="
+                        bg-[#FFFCF7]
+                        border-[#0D3B24]
+                        border-[1px]
+                        rounded-[15px]
+                        w-[550px]
+                        h-[50px]
+                        text-center
+                        font-lora
+                        text-[24px]
+                        text-[#0D3B24]
+                        outline-none
+                        mt-[15px]"
+                        placeholder="e-mail"
+                        type="email"
+                        value={form.email}
+                        onChange={(e) => setForm({...form, email: e.target.value})}
+                    />
+                    <input
+                        className="
+                        bg-[#FFFCF7]
+                        border-[#0D3B24]
+                        border-[1px]
+                        rounded-[15px]
+                        w-[550px]
+                        h-[50px]
+                        text-center
+                        font-lora
+                        text-[24px]
+                        text-[#0D3B24]
+                        outline-none
+                        mt-[15px]"
+                        placeholder="password"
+                        type="password"
+                        value={form.password}
+                        onChange={(e) => setForm({...form, password: e.target.value})}
+                    />
+                    <button
+                        type = "submit"
+                        className="mt-[30px] text-center border-[#0D3B24] border-[1px] rounded-[15px] bg-[#445C32] w-[350px] h-[50px] font-lora font-bold text-[20px] text-[#FFFFFF]
+                                    hover:cursor-pointer hover:bg-[#0D3B24] duration-500 "
+                    >Sign up</button>
+                </div>
+            </form>
+        </div>
+    )
+}
+export default Register;
